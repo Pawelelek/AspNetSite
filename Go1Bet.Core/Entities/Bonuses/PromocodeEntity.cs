7697a05550da660dfeb58096236511b052d593c5
unknown <slobodeniuk47@gmail.com>
2024-08-17 19:06:01 +0200

    Added Bonus Entities with User

--- Go1Bet.Core/Entities/Bonuses/PromocodeEntity.cs
+++ Go1Bet.Core/Entities/Bonuses/PromocodeEntity.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace Go1Bet.Core.Entities.Bonuses
 {
-    internal class PromocodeEntity
+    public class PromocodeEntity
     {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string Name { get; set; }
+        public DateTime DateCreated { get; set; } = DateTime.UtcNow;
+        public DateTime ExpirationDate { get; set; }
+        public string Key { get; set; }
+        public double PriceMoney { get; set; }
+        public int CountAvailable { get; set; }
+        public int CountEntries { get; set; }
     }
 }
--- Go1Bet.Core/Entities/Category/CategoryEntity.cs
+++ Go1Bet.Core/Entities/Category/CategoryEntity.cs
@@ -12,12 +12,13 @@ namespace Go1Bet.Core.Entities.Category
     [Table("tbl_Categories")]
     public class CategoryEntity
     {
-        public string Id { get; set; }
+        public string Id { get; set; } = Guid.NewGuid().ToString();
         public string Name { get; set; }
         [StringLength(4000)]
         public string Description { get; set; }
         [StringLength(255)]
         public string Image { get; set; }
+        public DateTime DateCreated { get; set; } = DateTime.UtcNow;
 
         [DisplayName("Category")]
         //Every category have a category
